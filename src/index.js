9a7699b303463185939cd1bf70f5fe6d9b8ab379
Chitransh Soni <chitransh7017soni@gmail.com>
2024-09-12 18:16:32 +0530

    second commit

---
+++ src/index.js
@@ -0,0 +1,2 @@
+//  difference between Dependencies and Dev Dependencies ?
+//  Dev Dependencies are those dependencies jinko hum sirf development me use krte h or unhe production me nahi leke jate h .
